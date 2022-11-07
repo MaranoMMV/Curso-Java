126a93b9dadee436059a9d6024cb7410c4452160
MaranoArt <77554696+MaranoMMV@users.noreply.github.com>
2022-11-07 16:42:50 -0300

    Add files via upload

---
+++ TesteProcessamento/src/Index/Main.java
@@ -0,0 +1,32 @@
+package Index;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*//Atributos
+		int a, b;
+		double resultado;
+		
+		//Construtor
+		a = 5;
+		b = 2;
+		
+		//Métodos
+		resultado = (double) a / b ;
+		
+		System.out.println(resultado);
+		System.out.println("\n");
+		*/
+		
+		//Atributos
+		double a;
+		int b;
+		//Construtor
+		a = 5.0;
+		b = (int) a;
+		
+		System.out.println(b);
+		
+		
+	}
+}
