b4c00a7ab5c38ffe0d4ea4422d0bb807ebeb6bf0
MaranoArt <77554696+MaranoMMV@users.noreply.github.com>
2023-04-26 17:46:46 -0300

    Add files via upload

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	
+}
---
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	
+}
